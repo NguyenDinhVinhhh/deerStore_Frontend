140462574e55d90036e06ef196841734cc1bd733
Hoang Hieu <hoanghieu@NguyenDinhVinh.lan>
2025-11-05 13:24:32 +0700

    fix: remove backend code mistakenly pushed into frontend

--- src/main/java/com/example/quanlycuahang/service/VaiTro/VaiTroService.java
+++
@@ -1,9 +0,0 @@
-package com.example.quanlycuahang.service.VaiTro;
-
-import com.example.quanlycuahang.entity.VaiTro.VaiTro;
-import java.util.List;
-
-public interface VaiTroService {
-    List<VaiTro> getAll();
-    VaiTro save(VaiTro vaiTro);
-}
